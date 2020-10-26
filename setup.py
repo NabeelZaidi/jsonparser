a4331b9743e704a7e1cc045ec1bd5a8532e96382
NabeelZaidi <37018555+NabeelZaidi@users.noreply.github.com>
2020-10-26 17:18:17 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='jsonparser',
+    version='0.0.1',
+    description = 'Process json and convert it into dataframes',
+    py_modules=["jsonparser"],
+    package_dir={'':'src'},
+    
+)
